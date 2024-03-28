84f10e682b548cb15cffc2e93da494689fa5ecc8
gitcodeur <165089430+gitcodeur@users.noreply.github.com>
2024-03-28 19:53:02 +0100

    page2.js

--- page2.js
+++ page2.js
@@ -1,3 +1,29 @@
+
+i = 0
+m = 0
+
+id = "123"
+mdp = "789"
+
 function hello(){
-    alert('bjr')
+    let baliseidt = document.getElementById("idt")
+    let idt = baliseidt.value
+
+    let balisepsrd = document.getElementById("psrd")
+    let psrd = balisepsrd.value
+
+    if(idt == id)
+        i = 1
+        console.log(i)
+
+    if(psrd == mdp)
+        m = 1
+        console.log(m)
+
+    if(i == m)
+        console.log('good')
+        window.open("main2.py","_blank", null);
 }
+
+
+
